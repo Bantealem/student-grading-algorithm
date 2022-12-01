c6fc0a94737f39a71b3caf802c7af0e649355fa7
Bantealem Geto <bantem2021@gmail.com>
2022-12-01 10:51:42 +0300

    Update challenges.js

--- challenges.js
+++ challenges.js
@@ -5,3 +5,6 @@ function gradingStudents(grades) {
     });
     return finalGrades;
 }
+
+If the difference between the  and the next multiple of  is less than , round  up to the next multiple of .
+If the value of  is less than , no rounding occurs as the result will still be a failing grade.
